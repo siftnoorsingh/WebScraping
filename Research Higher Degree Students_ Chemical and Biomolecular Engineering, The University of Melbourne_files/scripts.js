08cf5a2685758f22e8a2941c509df760796882fb
Siftnoor Singh <sift.singh@gmail.com>
2016-05-06 23:55:15 +1000

    Added files via upload

---
+++ Research Higher Degree Students_ Chemical and Biomolecular Engineering, The University of Melbourne_files/scripts.js
@@ -0,0 +1,34 @@
+<!-- // hide from old browsers
+
+function sendmail(address) {
+        document.write('<a href="\u006d\u0061\u0069\u006c\u0074\u006f\u003a'+address+'\u0040'+'unimelb.edu.au">');
+        document.write(address+'@unimelb.edu.au</a>');
+};
+
+
+function sendengmail(address) {
+        document.write('<a href="\u006d\u0061\u0069\u006c\u0074\u006f\u003a'+address+'\u0040'+'eng.unimelb.edu.au">');
+        document.write(address+'@eng.unimelb.edu.au</a>');
+}; 
+
+function sendwebmastermail(address) {
+        document.write('<a href="\u006d\u0061\u0069\u006c\u0074\u006f\u003a'+address+'\u0040'+'eng.unimelb.edu.au">');
+        document.write(address+'@eng.unimelb.edu.au</a>');
+};
+
+
+function sendmeritmail(address) {
+        document.write('<a href="\u006d\u0061\u0069\u006c\u0074\u006f\u003a'+address+'\u0040'+'merit.unimelb.edu.au">');
+        document.write(address+'@merit.unimelb.edu.au</a>');
+};
+
+function sendstudentmail(address) {
+        document.write('<a href="\u006d\u0061\u0069\u006c\u0074\u006f\u003a'+address+'\u0040'+'student.unimelb.edu.au">');
+        document.write(address+'@student.unimelb.edu.au</a>');
+};
+
+function sendpgradmail(address) {
+        document.write('<a href="\u006d\u0061\u0069\u006c\u0074\u006f\u003a'+address+'\u0040'+'pgrad.unimelb.edu.au">');
+        document.write(address+'@pgrad.unimelb.edu.au</a>');
+};
+// -->
